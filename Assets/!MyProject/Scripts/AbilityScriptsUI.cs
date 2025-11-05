ff1e17ddc7e625a9b674564e786c386e170c8cbb
Darkness <Boostgamedota2@yandex.ru>
2025-11-05 18:58:40 +0300

    buttonui

---
+++ Assets/!MyProject/Scripts/AbilityScriptsUI.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AbilityScriptsUI : MonoBehaviour
+{
+    public Button Ability1;
+    public Button Ability2;
+    public Button Ability3;
+
+
+
+    private void Start()
+    {
+        
+    }
+
+
+}

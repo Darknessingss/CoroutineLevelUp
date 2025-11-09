f0c16ab1bf49289e1933a0a3bbc9e9aaefe31a82
Darkness <Boostgamedota2@yandex.ru>
2025-11-09 18:20:06 +0300

    testUI

--- Assets/!MyProject/Scripts/AbilityScriptsUI.cs
+++ Assets/!MyProject/Scripts/AbilityScriptsUI.cs
@@ -1,18 +1,43 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using System;
+using System.Collections;
 
+
+[System.Serializable]
 public class AbilityScriptsUI : MonoBehaviour
 {
+    [Header("ButtonAbility")]
     public Button Ability1;
     public Button Ability2;
     public Button Ability3;
 
+    [Header("ButtonSettings")]
+    public int TimerAbility = 5;
+    public Image Icon;
+    public TMP_Text TextCouldown;
 
 
-    private void Start()
+    [Header("SettingsObject")]
+    [SerializeField] private GameObject Player;
+
+
+
+    public static class Ability1Spell
     {
-        
+        public static event Action<int> ChangeColorPlayer;
+    }
+
+    private void Awake()
+    {
+        Ability1Spell.ChangeColorPlayer += ChangePlayerMaterial;
     }
 
 
+    private void OnDestroy()
+    {
+        Ability1Spell.ChangeColorPlayer -= ChangePlayerMaterial;
+    }
 }
+
